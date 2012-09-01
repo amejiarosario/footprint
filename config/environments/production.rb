85ee6d052599c4a7945629d9722c59a220e181ab
Adrian Mejia <adriansky@gmail.com>
2012-09-01 10:09:44 -0400

    update new hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Footprint::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'footnote-2012.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'footprint-2012.herokuapp.com' }
 end
